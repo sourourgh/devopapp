96d9eb765ee676f898e66a5e187a4e0240d28020
sourourgh <159917235+sourourgh@users.noreply.github.com>
2024-04-22 09:20:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('You are  in AWS  Cloud. THis is TECHESSfghjklmfgvhjklmAY, Please sha555re and Subscribe and like my Videos'); //write a response to the client
+  res.write('You are  in AWS  Cloud. THis is TECHESSfghjklmfgvhjklmAY,112223 Please sha555re and Subscribe and like my Videos'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
 
